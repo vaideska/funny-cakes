9ce8c8030905411d6b3b9a63f32ac1425af7f81f
Anton Abramov(chippsi) <aabramov9696@gmail.com>
2022-02-09 21:54:17 +0300

    add style theme

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './components/App/App'
 import { BrowserRouter as Router } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/system';
+import { theme } from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
     <CssBaseline />
-    <Router>
-      <App />
-    </Router>
+    <ThemeProvider theme={theme} >
+      <Router>
+        <App />
+      </Router>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
