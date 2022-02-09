51c5103d59ae9e714ce8cc9c98139b6e5c66af7e
Anton Abramov(chippsi) <aabramov9696@gmail.com>
2022-02-09 17:09:21 +0300

    add normalize styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App'
 import { BrowserRouter as Router } from 'react-router-dom';
+import { CssBaseline } from '@mui/material';
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <Router>
       <App />
     </Router>
