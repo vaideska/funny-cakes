e9c7bb9ad6ec496a8060fc6b734ade53bfc50435
Evgenii Denisov <jackyapa6eu@yandex.ru>
2022-02-15 15:47:13 +0300

    local merge

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,56 +1,16 @@
 import React, {useCallback, useEffect, useState} from 'react';
 import {Route, Switch} from "react-router-dom";
 import { routes } from '../../utils/routes';
-<<<<<<< HEAD
-import {CreateRecipe} from "../CreateRecipe/CreateRecipe";
-=======
-import { initializeApp } from "firebase/app";
->>>>>>> main
 import {FullRecipe} from "../FullRecipe/FullRecipe";
 import { HeaderContainer } from '../../containers/HeaderContainer';
 import {RecipesFeedContainer} from "../../containers/RecipesFeedContainer";
 import {child, get, getDatabase, onValue, ref} from "firebase/database";
 import { AuthZModalContainer } from '../../containers/AuthZModalContainer';
 import { CreateRecipeFormContainer } from '../../containers/CreateRecipeContainer/CreateRecipeFormContainer';
-import { Recipe } from "../../types/recipeType";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import {login} from "../../store/slices/authZ/authZSlice";
 import {useAppDispatch} from "../../hooks/useAppDispatch";
 
-<<<<<<< HEAD
-interface RecipeIngredient {
-  name: string,
-  unit: string,
-  count: number
-}
-
-export interface Recipe {
-  id: string,
-  title: string,
-  description: string,
-  owner: string,
-  date: number,
-  duration: number,
-  diameter: number,
-  imgUrl: string,
-  tags: string[],
-  ingredients: RecipeIngredient[],
-  recipeText: string
-}
-=======
-(function() {
-  const firebaseConfig = {
-    apiKey: "AIzaSyCCalDHvx-N-OD-UE4P7XQKdjj3bdCnDN0",
-    authDomain: "napoleon-tech.firebaseapp.com",
-    databaseURL: "https://napoleon-tech-default-rtdb.firebaseio.com",
-    projectId: "napoleon-tech",
-    storageBucket: "napoleon-tech.appspot.com",
-    messagingSenderId: "674037683443",
-    appId: "1:674037683443:web:abc96f116192681c5a5386"
-  };
-  const app = initializeApp(firebaseConfig)
-})();
->>>>>>> main
 
 function App() {
   const dispatch = useAppDispatch();
