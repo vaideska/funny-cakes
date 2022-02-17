667bffb59154cba667fb49bcc39f8bc757ae0ba0
DESKTOP-K0HS5QI\Dell <vaideska@mail.ru>
2022-02-17 23:59:41 +0300

    refactoring

--- src/components/FullRecipe/FullRecipe.tsx
+++ src/components/FullRecipe/FullRecipe.tsx
@@ -2,14 +2,28 @@ import React, {useEffect, useState} from "react";
 import {child, get, getDatabase, ref} from "firebase/database";
 import {Recipe} from "../../types/recipeType";
 import {useRouteMatch} from "react-router-dom";
+import { RecipePrintComponent } from '../RecipePrint';
 
 interface MatchParams {
   id: string;
 }
 
 export const FullRecipe = (): React.ReactElement => {
+  const initStateForm: Recipe = {
+    id: "",
+    title: "",
+    description: "",
+    owner: {lastName: 'kek', firstName: 'lol', id: '', profile_picture: '', email: ''},
+    date: Date.now(),
+    duration: 0,
+    diameter: 0,
+    imgUrl: "",
+    tags: [],
+    ingredients: [],
+    recipeText: ""};
+
   const routeMatch = useRouteMatch<MatchParams>();
-  const [recipe, setRecipe] = useState<Recipe | undefined>()
+  const [recipe, setRecipe] = useState<Recipe>(initStateForm);
   useEffect(() => {
     if (routeMatch.params.id) {
       getRecipe();
@@ -40,6 +54,7 @@ export const FullRecipe = (): React.ReactElement => {
       <p>{recipe?.title}</p>
       <p>{recipe?.description}</p>
       <p>{`${recipe?.owner.firstName} ${recipe?.owner.lastName}`}</p>
+      <RecipePrintComponent recipe={recipe} />
     </div>
   )
 }
--- src/components/RecipePrint/RecipePrintComponent.tsx
+++ src/components/RecipePrint/RecipePrintComponent.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import style from './RecipePrintComponent.module.css';
 import { Recipe } from '../../types/recipeType';
 import { unitList } from '../../utils/dictionary';
+import ReactToPrint from 'react-to-print';
+import { Button } from '@mui/material';
 
 interface PrintComponentProps {
   recipe: Recipe,
@@ -12,18 +14,18 @@ const PrintComponent = React.forwardRef(({ recipe }: PrintComponentProps, ref: R
 
   return (
     <div className={style.print} ref={ref}>
-      <h1>{title}</h1>
+      <h3>{title}</h3>
       <p>{`Автор: ${owner.firstName} ${owner.lastName}`}</p>
       <p>{description}</p>
-      <p>Время приготовления: {duration} минут</p>
-      <p>Диаметр торта: {diameter}</p>
-      <h2>Состав:</h2>
+      <p>Время приготовления: {duration} мин.</p>
+      <p>Диаметр торта: {diameter} см.</p>
+      <h4>Состав:</h4>
       <ul>
         {ingredients.map(({ name, unit, count}, index) => {
           return <li key={index}>{`${name} — ${count} ${unitList[unit]}`}</li>
         })}
       </ul>
-      <h2>Описание рецепта:</h2>
+      <h4>Инструкция приготовления:</h4>
       <p>{recipeText}</p>
     </div>
   );
@@ -31,5 +33,11 @@ const PrintComponent = React.forwardRef(({ recipe }: PrintComponentProps, ref: R
 
 export const RecipePrintComponent = ({ recipe }: PrintComponentProps) => {
   const componentRef = React.useRef(null);
-  return <PrintComponent recipe={recipe} ref={componentRef} />;
+  return (<>
+  <ReactToPrint
+    trigger={() => <Button variant="contained">Напечатать рецепт</Button>}
+    content={() => componentRef.current}
+  />
+  <PrintComponent recipe={recipe} ref={componentRef} />
+  </>)
 };
