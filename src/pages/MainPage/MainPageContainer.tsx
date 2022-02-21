704a69fd1b897628e386778ce166d1e4b86dd9ec
DESKTOP-K0HS5QI\Dell <vaideska@mail.ru>
2022-02-21 20:59:00 +0300

    delete console.log

--- src/pages/MainPage/MainPageContainer.tsx
+++ src/pages/MainPage/MainPageContainer.tsx
@@ -24,7 +24,6 @@ export const MainPageContainer = () => {
 
   const handleChange = useCallback((event: SelectChangeEvent<string[]>) => {
     const value = event.target.value;
-    console.log(value);
     setTags(() => {
         const newTags = typeof value === 'string' ? value.split(',') : value;
         return newTags;
